e45257da4f6907e8caf146cb42f947cbc1f977f4
maticavallo <maticavallo@gmail.com>
2023-01-20 12:13:04 +0100

    first commit

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,33 +1,38 @@
-// I'm a comment!
 // SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.7;
-// pragma solidity ^0.8.0;
-// pragma solidity >=0.8.0 <0.9.0;
+pragma solidity ^0.8.17;
 
 contract SimpleStorage {
 
+    // A variable to store a favorite number
     uint256 favoriteNumber;
 
+    // A struct to store people with a name and favorite number
     struct People {
         uint256 favoriteNumber;
         string name;
     }
     // uint256[] public anArray;
+    // An array to store multiple people
     People[] public people;
 
+    // A mapping to map a name to a favorite number
     mapping(string => uint256) public nameToFavoriteNumber;
 
+    // A function to store a favorite number
     function store(uint256 _favoriteNumber) public virtual {
         favoriteNumber = _favoriteNumber;
     }
     
+    // A function to retrieve the stored favorite number
     function retrieve() public view returns (uint256){
         return favoriteNumber;
     }
 
+    // A function to add a person to the people array and mapping
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        // adds a new person to the people array
         people.push(People(_favoriteNumber, _name));
+        // adds a new entry to the name to favorite number mapping
         nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,27 +1,30 @@
 // SPDX-License-Identifier: MIT 
-
-pragma solidity ^0.8.7;
-
+pragma solidity ^0.8.17;
 import "./SimpleStorage.sol"; 
 
+// This contract is a factory contract that creates and manages multiple instances of SimpleStorage contract
 contract StorageFactory {
     
+    // An array to store multiple SimpleStorage contract instances
     SimpleStorage[] public simpleStorageArray;
     
+    // A function to create a new instance of SimpleStorage contract
     function createSimpleStorageContract() public {
+        // Creating a new instance of SimpleStorage contract
         SimpleStorage simpleStorage = new SimpleStorage();
+        // Storing the new instance in the simpleStorageArray
         simpleStorageArray.push(simpleStorage);
     }
     
+    // A function to store a number in a specific SimpleStorage contract instance
     function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
-        // Address 
-        // ABI 
-        // SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+        // Store the number in the specified SimpleStorage contract instance
         simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
     }
     
-    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
-        // return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+    // A function to retrieve a stored number from a specific SimpleStorage contract instance
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) { 
+        // Retrieve the stored number from the specified SimpleStorage contract instance
         return simpleStorageArray[_simpleStorageIndex].retrieve();
     }
 }
